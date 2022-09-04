4022e1463f84b0a5b8b8b6ae3f1cda9479884be9
SalMaN <kfoo2023@gmail.com>
2022-09-04 22:55:47 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,93 @@
+(function ($) {
+
+	"use strict";
+
+	$(function () {
+		$("#tabs").tabs();
+	});
+
+	$(window).scroll(function () {
+		const scroll = $(window).scrollTop();
+		const box = $('.header-text').height();
+		const header = $('header').height();
+
+		if (scroll >= box - header) {
+			$("header").addClass("background-header");
+		} else {
+			$("header").removeClass("background-header");
+		}
+	});
+
+
+	$('.schedule-filter li').on('click', function () {
+		const tsfilter = $(this).data('tsfilter');
+		$('.schedule-filter li').removeClass('active');
+		$(this).addClass('active');
+		if (tsfilter == 'all') {
+			$('.schedule-table').removeClass('filtering');
+			$('.ts-item').removeClass('show');
+		} else {
+			$('.schedule-table').addClass('filtering');
+		}
+		$('.ts-item').each(function () {
+			$(this).removeClass('show');
+			if ($(this).data('tsmeta') == tsfilter) {
+				$(this).addClass('show');
+			}
+		});
+	});
+
+
+	// Window Resize Mobile Menu Fix
+	mobileNav();
+
+	// Window Resize Mobile Menu Fix
+	$(window).on('resize', function () {
+		mobileNav();
+	});
+
+	// Menu Dropdown Toggle
+	if ($('.menu-trigger').length) {
+		$(".menu-trigger").on('click', function () {
+			$(this).toggleClass('active');
+			$('.header-area .nav').slideToggle(200);
+		});
+	}
+
+	// Window Resize Mobile Menu Fix
+	function mobileNav() {
+		const width = $(window).width();
+		$('.submenu').on('click', function () {
+			if (width < 767) {
+				$('.submenu ul').removeClass('active');
+				$(this).find('ul').toggleClass('active');
+			}
+		});
+	}
+
+
+})(window.jQuery);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener('click', (e) => {
+		e.preventDefault();
+		// scroll smoothly to the element
+		document.querySelector(e.target.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth'
+		});
+	});
+});
+
+window.onload = () => {
+	//hide the preloader
+	document.getElementById("js-preloader").classList.add('loaded')
+}
+
+const contact_form = document.getElementById("contact");
+
+contact_form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const subject = document.getElementById("subject");
+	subject.value = "Form Submission: " + subject.value;
+	contact_form.submit()
+});
